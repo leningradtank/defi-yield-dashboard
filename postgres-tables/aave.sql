a42d39e5e82c2042946fad423f4b0ebff223e08d
ridersamz2000 <47441021+ridersamz2000@users.noreply.github.com>
2022-06-03 04:05:14 -0500

    first commit iteration for docker

---
+++ postgres-tables/aave.sql
@@ -0,0 +1,3 @@
+CREATE TABLE aave_yield( name TEXT, depositAPR FLOAT, variableBorrowAPR FLOAT, stableBorrowAPR FLOAT, depositAPY FLOAT, variableBorrowAPY FLOAT, stableBorrowAPY FLOAT, timestamp TIMESTAMP );
+
+
